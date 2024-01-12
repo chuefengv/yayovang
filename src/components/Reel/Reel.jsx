4bdd090198702be06ecf05deb438e4dd8b07017b
chuefengv <feng.vang98@gmail.com>
2024-01-11 22:40:30 -0500

    added reel to page

---
+++ src/components/Reel/Reel.jsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+import "../../app/globals.css"
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
+
+export default function Reel() {
+  const URL = 'https://player.vimeo.com/video/836371548?h=551c266867';
+
+  return (
+    <div className='reel'>
+      <div className='reel-wrapper'>
+        <ReactPlayer
+          className='react-player'
+          width='100%'
+          height='100%'
+          controls={true}
+          url={URL}
+        />
+      </div>
+    </div>
+  );
+}
