611287a69a02100b472d68594696a327c87b7326
chuefengv <feng.vang98@gmail.com>
2024-03-08 14:26:04 -0800

    save

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React,{useState} from 'react'
 import "../../app/globals.css"
 import { projectlist } from '@/app/projectlist';
 import "../../../node_modules/react-modal-video/scss/modal-video.scss"
