04aa474c8afdf8d028f6527db9bad8df5598ca16
chuefengv <feng.vang98@gmail.com>
2024-04-22 17:33:55 -0700

    remove reactplayer thumbnail

--- src/app/demoreel/page.jsx
+++ src/app/demoreel/page.jsx
@@ -19,7 +19,7 @@ export default function demoreel() {
           controls={true}
           width='100%'
           height='100%'
-          light='https://yayovangprojects.s3.amazonaws.com/images/reel_thumbnail.webp'
+          // light='https://yayovangprojects.s3.amazonaws.com/images/reel_thumbnail.webp'
         />
       </div>
 
